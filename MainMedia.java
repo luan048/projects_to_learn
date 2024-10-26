3ec6a939c72f2f1a987a0be5293fa64f72976ee2
luan048 <luanmonteiropedrosa@gmail.com>
2024-10-26 00:00:48 -0300

    Update in Media

--- MainMedia.java
+++ MainMedia.java
@@ -1,6 +1,6 @@
 public class MainMedia {
     public static void main(String[] args) {
         Media media = new Media();
-        media.media(5.5, 7.9, 8.0, 10);
+        media.media(5.5, 2.3, 1.1, 10); 
     }
 }
--- Media.java
+++ Media.java
@@ -1,8 +1,12 @@
 public class Media {
-
     public void media(double nota1, double nota2, double nota3, double nota4) {
         double media = (nota1 + nota2 + nota3 + nota4) / 4;
-        
-        System.out.println(String.format("%.1f", media));
+
+        if(media < 7) {
+            System.out.println("Reprovado! Com media de: " + String.format("%.1f", media));
+        }
+        else {
+            System.out.println("Aprovado! Com média de: " + String.format("%.1f", media) + " Parabéns! ");
+        }
     }
 }
